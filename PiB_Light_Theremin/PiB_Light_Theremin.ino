abd4c39155c9b739a6b4be88e8a99a0d88dc5de5
a1xu <a1xu@ucsd.edu>
2017-11-08 15:02:54 -0800

    Light to frequency mapping

--- PiB_Light_Theremin/PiB_Light_Theremin.ino
+++ PiB_Light_Theremin/PiB_Light_Theremin.ino
@@ -1,10 +1,14 @@
 // Pin assignments //
 const int buzzer = 8;
-const int toggle_switch = 7
+const int toggle_switch = 7;
 const int photocell = A0;
 
 // Initialize values //
-int freq = 1000;
+int freq;
+int min_freq = 100;
+int max_freq = 10000;
+int min_light = 0;
+int max_light = 1000;
 int light_val = 0;
 int switch_state = 0;
 
@@ -12,6 +16,7 @@ void setup() {
   // Pin mode assignment //
   pinMode(buzzer, OUTPUT);
   pinMode(toggle_switch, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -22,7 +27,7 @@ void loop() {
   light_val = analogRead(photocell);
 
   // Calculate output frequency //
-  freq = 200 + light_val / 4;
+  freq = map(light_val, min_light, max_light, min_freq, max_freq);
 
   if(switch_state == HIGH){
     // Update buzzer pitch //
@@ -33,4 +38,12 @@ void loop() {
   else{
     noTone(buzzer);
   }
+
+//  Serial.print("Photocell: ");
+//  Serial.print(light_val);
+//  Serial.print(", Frequency: ");
+//  Serial.print(freq);
+//  Serial.print("\n");
+//
+  delay(100);
 }
